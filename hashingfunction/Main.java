ea7d9d26564e7da2272f1088569b33eda4db347b
akshay dhangar <akshaygdhangar14@gmail.com>
2022-04-09 18:59:50 +0530

    Added

---
+++ hashingfunction/Main.java
@@ -0,0 +1,14 @@
+package hashingfunction;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		HashFunc func = new HashFunc();
+		func.hashTable();
+		func.showTable();
+		func.fileWrite();
+
+	}
+
+
+}
