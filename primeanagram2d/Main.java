825cdba8b0cfa82873b58e3a80d82b6a1ea5a460
akshay dhangar <akshaygdhangar14@gmail.com>
2022-04-09 18:43:50 +0530

    Added

---
+++ primeanagram2d/Main.java
@@ -0,0 +1,13 @@
+package primeanagram2d;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PrimeAnagram primeAnagram = new PrimeAnagram();
+		primeAnagram.anagramNo();
+		primeAnagram.show();
+	}
+
+}
