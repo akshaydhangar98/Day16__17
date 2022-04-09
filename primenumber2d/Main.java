1cc30e57fdf17f3b38105c787482620803ab396d
akshay dhangar <akshaygdhangar14@gmail.com>
2022-04-09 18:48:51 +0530

    Added

---
+++ primenumber2d/Main.java
@@ -0,0 +1,12 @@
+package primenumber2d;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PrimeTwoDim primeNo = new PrimeTwoDim();
+		primeNo.primeNo();
+		primeNo.show();
+
+	}
+
+}
