815d210ba1bec27316f01327ee06048f81065e71
akshay dhangar <akshaygdhangar14@gmail.com>
2022-04-09 18:36:43 +0530

    Added

---
+++ balancedparantheses/Main.java
@@ -0,0 +1,14 @@
+package balancedparantheses;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the expression:");
+		String input = scan.nextLine();
+		BalancedParantheses<String> balancedParantheses = new BalancedParantheses<String>(input);
+		System.out.println(balancedParantheses.checkParanthesis());
+	}
+}
---
+++ balancedparantheses/Node.java
@@ -0,0 +1,14 @@
+package balancedparantheses;
+
+public class Node {
+	
+	String element;
+	Node next;
+	public Node(String element) {
+		this.element = element;
+		this.next = null;
+		BalancedParantheses.size++;
+	}
+	
+
+}
